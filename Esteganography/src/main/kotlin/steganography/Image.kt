47d747a13b7dc25c317864eb57f53e7173ee757a
Azakhy <321049393@ciencias.unam.mx>
2024-10-16 17:00:47 -0600

    Image and Text documentation

---
+++ Esteganography/src/main/kotlin/steganography/Image.kt
@@ -0,0 +1,10 @@
+import java.awt.image.BufferedImage
+/**
+ * Image
+ *
+ * This class process an image in format png and ___.
+ *
+ * @property image The image to be processed.
+ */
+class ImageHandler(private val image : BufferedImage) {
+}
---
+++ Esteganography/src/main/kotlin/steganography/Text.kt
@@ -0,0 +1,9 @@
+/**
+ * Text
+ * 
+ * This class proccess a text so as to obtain a Strings list
+ *
+ * @property text The text to be processed.
+ */
+class TextHandler(private val text: String) {
+}
