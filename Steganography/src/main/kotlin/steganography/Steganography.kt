358755b594671cdc4e91b9de794e626eb7cffd07
Gibran-RL <gibranramirez@ciencias.unam.mx>
2024-10-17 14:27:36 -0600

    :memo: Reubicate read and save image from main.kt to image.kt

--- Steganography/src/main/kotlin/steganography/Steganography.kt
+++ Steganography/src/main/kotlin/steganography/Steganography.kt
@@ -59,24 +59,4 @@ class Steganography {
         // Implementation
     }
 
-    /**
-     * Loads an image from the specified file path.
-     *
-     * @param filePath The path of the image file.
-     * @return The loaded image object.
-     */
-    fun loadImage(filePath: String): Image {
-        // Implementation
-    }
-
-    /**
-     * Saves the modified image to the specified file path.
-     *
-     * @param image The image to be saved.
-     * @param filePath The path where the image should be saved.
-     * @return True if the image was saved successfully, otherwise false.
-     */
-    fun saveImage(image: Image, filePath: String): Boolean {
-        // Implementation
-    }
 }
