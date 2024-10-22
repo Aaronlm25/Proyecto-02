08a27ec13594288f54c0cf6fa1993bc796358eab
Azakhy <321049393@ciencias.unam.mx>
2024-10-21 19:33:26 -0600

    Two tests have been added to the Steganography class.

--- Steganography/src/test/kotlin/seteganography/SteganographyTest.kt
+++ Steganography/src/test/kotlin/seteganography/SteganographyTest.kt
@@ -5,14 +5,29 @@ import steganography.decodeText
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.kotest.assertions.throwables.shouldThrow
+import java.io.File
 
 class SteganographyTest : StringSpec ({
-    "should encode text be clean" {
-        0 shouldBe 0
+    "should throw exception when text contains invalid characters during encoding" {
+        val path = "src/test/resources/invalidText.txt"
+        val file = File(path)
+        val text = file.readText()
+        shouldThrow<IllegalArgumentException> {
+            val pixels = arrayOf(intArrayOf(0))
+            encodeText(text.toList(), pixels)
+        }
     }
 
-    "should decode text correctly" {
-        0 shouldBe 0
+    "should not handle multiple encodings" {
+        val text1 = "First encoding"
+        val text2 = "Second encoding"
+        val pixels = Array(100) { IntArray(100) }
+        val encodedImage1 = encodeText(text1.toList(), pixels)
+        val encodedImage2 = encodeText(text2.toList(), encodedImage1)
+        val decodedText = decodeText(encodedImage2)
+
+        decodedText shouldBe text2.toList()
     }
 
     "should encode text correctly into the pixel array" {
