a62f9b949ae3d6a4d4795996d130648ed9e1e8d8
Azakhy <321049393@ciencias.unam.mx>
2024-10-28 00:06:47 -0600

    Unnecessary imports were removed from the tests

--- Steganography/src/test/kotlin/seteganography/data/text/TextTest.kt
+++ Steganography/src/test/kotlin/seteganography/data/text/TextTest.kt
@@ -2,10 +2,8 @@ package steganography.data.text
 
 import steganography.data.text.readFile
 import steganography.data.text.toFile
-import steganography.data.text.getRegulars
 import steganography.data.text.readFull
-import steganography.data.text.reassemble
-import steganography.data.text.replaceAlphabet
+import steganography.data.text.compress
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.assertions.throwables.shouldThrow
