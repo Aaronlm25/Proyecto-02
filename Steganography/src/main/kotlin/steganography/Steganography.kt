5e7e6d9bc7694ceff4273886305d4e078afb4438
Azakhy <321049393@ciencias.unam.mx>
2024-11-03 14:31:45 -0600

    Second implementation of encoding

--- Steganography/src/main/kotlin/steganography/Steganography.kt
+++ Steganography/src/main/kotlin/steganography/Steganography.kt
@@ -8,42 +8,68 @@ import java.awt.image.BufferedImage
  * Encodes the text into an image represented by an integer array of the pixels.
  *
  * @param text The text to be encoded.
- * @param pixels The 2D pixel array of the image.
- * @return 2D pixel array with the message encoded.
+ * @param pixels Contains the pixels that make up the image.
+ * @return An object of type BufferedImage.
  * @throws IllegalStateException if the text is too large for the pixels array.
  */
-fun encodeText(text: List<Char>, image: BufferedImage): BufferedImage {
-    val seed = image.getRGB(0, 0).toLong()
-    val algorithm = Random(seed)
-    val textValues = replaceAlphabet(text)
+fun encodeText(text: List<Char>,pixels: BufferedImage): BufferedImage {
+    //width ancho, Columas
+    val algorithm = Random(pixels.getRGB(0, 0).toLong())
+    val textValues = replaceAlphabet(text) //Lista de enteros
     val binaries = textValues.map { it.toString(2).padStart(6, '0') }
-    /**
     var current = 0
     val modifiedPixels = mutableSetOf<Pair<Int,Int>>() //Pixeles que se vayan modificando.
+    val totalPixels = pixels.width*pixels.height //Numero total de pixeles
+    
     for (binary in binaries) { //Los binarios son un Strinf (No me funen).
-        for (bitChar in binary) { //Recorre los chars que forman al binario que es un String.
+        for (bitChar in binary.indices step(2)) { //Recorre los chars que forman al binario que es un String de dos en dos.
             if (current >= totalPixels) break //No se pueden agregar mas bits que pixeles en la imagen.
-            val x = algorithm.nextInt(pixels[0].size) //Valor de la columna
-            val y = algorithm.nextInt(pixels.size) //Valor de la fila
+
+            // Extraer los bits de dos en dos
+            val bitPair = if (bitChar + 1 < binary.length) {
+                binary.substring(bitChar, bitChar + 2) // Obtiene el par de bits, revisar doc de Substring si hay dudas.
+            } else {
+                binary[bitChar].toString() // Si solo hay un bit
+            }
+
+           // Guardar cada bit en variables separadas como Int
+            val bit1 = bitPair[0].digitToInt() // Convierte el primer bit a Int
+            val bit2 = if (bitPair.length > 1) bitPair[1].digitToInt() else 0 // Convierte el segundo bit a Int o 0 si solo hay uno
+
+            val x = algorithm.nextInt(pixels.width) //Valor de la columna
+            val y = algorithm.nextInt(pixels.height) //Valor de la fila
+
             if (Pair(y, x) in modifiedPixels) continue //Evita que el random caiga en un pixel ya visitado.
+            
             modifiedPixels.add(Pair(y,x))//Se anade el pixel visitado.
-            val rgb = pixels[y][x] //componente i,j-esima
-            val red = (rgb shr 16) and 0xFF
+
+            val rgb = pixels.getRGB(y, x) //componente i,j-esima
+            val alpha = (rgb shr 24) and 0xFF //corrimiento de bits y mascaras para filtrar los 8 ultimos bits
+            val red = (rgb shr 16) and 0xFF 
             val green = (rgb shr 8) and 0xFF
             val blue = rgb and 0xFF
-            val bit = bitChar.toString().toInt() //En principio los elementos son Char.
-            val newBlue = //Se modifica el valor en Blue.
-            if ((blue and 1) == bit) {//Matching (Solo cambios necesarios).
-                blue
-            } else {
-                if (blue == 0) 1 else blue - 1
-            }
-            val newRgb = (red shl 16) or (green shl 8) or newBlue
-            pixels[y][x] = newRgb
+            
+            val newBlue = channel(blue, bit1)//Guarda el 1er bit en blue
+            val newAlpha = channel(alpha, bit2)//Guarda el 2do bit en alpha
+            val newRgb = (newAlpha shl 24) or (red shl 16) or (green shl 8) or newBlue
+            pixels.setRGB(x, y, newRgb) //Modificamos el valor del pixel guardando la infromacion desada
             current++ //Indicamos que se va a usar otro pixel.
         }
-    }*/
-    return image
+    }
+    return pixels
+}
+/**
+ * Modifies the LSB of the desired channel.
+ * @param channel The channel to be modified.
+ * @param bit The bit to be compared with the channel's LSB.
+ * @return The modified channel.
+ */
+private fun channel(channel: Int, bit: Int): Int {
+    return if ((channel and 1) == bit) {
+        channel
+    } else {
+        if (channel == 0) 1 else channel - 1
+    }
 }
 
 private val reverseAlphabet = mapOf(
